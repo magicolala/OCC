1a264a034e56777726f77df3a452499f088940e4
Cédric Oloa <cedric.oloa@laposte.net>
2016-07-04 20:56:23 +0200

    Création du template

--- src/Bookeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookeeper/ManagerBundle/Controller/BookController.php
@@ -8,19 +8,19 @@ use Symfony\Component\HttpFoundation\Request;
 class BookController extends Controller {
 
     public function indexAction() {
-
+        return $this->render('BookeeperManagerBundle:Book:index.html.twig');
     }
     public function showAction($id) {
-
+        return $this->render('BookeeperManagerBundle:Book:show.html.twig');
     }
-    public function newAction($id) {
-
+    public function newAction() {
+        return $this->render('BookeeperManagerBundle:Book:new.html.twig');
     }
     public function createAction(Request $request) {
 
     }
     public function editAction($id) {
-
+        return $this->render('BookeeperManagerBundle:Book:edit.html.twig');
     }
     public function updateAction(Request $request, $id) {
 
